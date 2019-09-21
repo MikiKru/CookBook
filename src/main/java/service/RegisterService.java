e0a2f86e6a7d65c5d44b3de16f97a63d6a7b8a40
MikiKru <michal_kruczkowski@o2.pl>
2019-09-21 10:57:07 +0200

    d5 v5

--- src/main/java/service/RegisterService.java
+++ src/main/java/service/RegisterService.java
@@ -1,8 +1,13 @@
 package service;
 
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import model.User;
+import utility.InMemoryDB;
+
+import java.util.Optional;
 
 public class RegisterService {
     public void showPassword(
@@ -27,5 +32,33 @@ public class RegisterService {
             pf_password.setText(tf_password.getText());
             pf_password_confirm.setText(tf_password_confirm.getText());
         }
+    }
+    public boolean passwordsEquality(
+            CheckBox cb_showPassword, TextField tf_password, PasswordField pf_password,
+            TextField tf_password_confirm, PasswordField pf_password_confirm, Label lbl_error){
+        if(cb_showPassword.isSelected()){
+            if(!tf_password.equals(tf_password_confirm)){
+                lbl_error.setText("hasła nie są jednakowe");
+                return false;
+            }
+            return true;
+        } else {
+            if(!pf_password.equals(pf_password_confirm)){
+                lbl_error.setText("hasła nie są jednakowe");
+                return false;
+            }
+            return true;
+        }
+    }
+    public boolean loginCheck(TextField tf_login, Label lbl_error){
+        // sprawdzenie czy dany login występuje w bazie danych
+        Optional<User> user_valid = InMemoryDB.users.stream()
+                                    .filter(user -> user.getLogin().equals(tf_login.getText()))
+                                    .findAny();
+        if(user_valid.isPresent()){
+            lbl_error.setText("Istnieje już użytkownik o podanym loginie");
+            return false;
+        }
+        return true;
     }
 }
